f857756972b890aa522e94301df37e169c9def57
teyyihan <teyyihanaksu1@gmail.com>
2020-09-08 00:26:15 +0300

    SplashFragment session check

--- app/src/main/java/com/teyyihan/e2ee_chat/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/teyyihan/e2ee_chat/ui/splash/SplashFragment.kt
@@ -1,19 +1,21 @@
 package com.teyyihan.e2ee_chat.ui.splash
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.teyyihan.core.base.BaseActivity
 import com.teyyihan.core.base.BaseFragment
+import com.teyyihan.data.model.UserLocal
+import com.teyyihan.e2ee_chat.R
 import com.teyyihan.e2ee_chat.databinding.FragmentSplashBinding
-import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collectLatest
 
 
 class SplashFragment : BaseFragment() {
+
+    private val TAG = "teooo SplashFragment"
 
     private lateinit var binding: FragmentSplashBinding
     private val viewModel by viewModels<SplashViewModel>()
@@ -24,8 +26,20 @@ class SplashFragment : BaseFragment() {
     ): View? {
         binding= FragmentSplashBinding.inflate(inflater,container,false)
 
+        (activity as BaseActivity).sessionManager.cachedUser.observe(viewLifecycleOwner){
+            sessionCheck(it)
+        }
 
         return binding.root
+    }
+
+    private fun sessionCheck(userLocal: UserLocal?) {
+        if(userLocal == null || userLocal.isRefreshTokenExpired()){
+            findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+        }else{
+            //TODO: Refresh access token, if failed, go to login fragment
+            //findNavController().navigate(R.id.action_splashFragment_to_mainFragment)
+        }
     }
 
 
